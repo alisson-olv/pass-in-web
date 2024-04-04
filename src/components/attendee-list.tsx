d8f8cba830b88cbfb6660a6f1673fd75188fc6d8
Alisson Oliveira <binho_alisson@hotmail.com>
2024-04-03 21:46:00 -0300

    feat: creating components and formatting dates

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,7 +1,16 @@
 import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronsRight, ChevronRight } from 'lucide-react';
+import { ptBR } from "date-fns/locale";
+import { formatRelative } from "date-fns/formatRelative";
 import { IconButton } from './icon-button';
+import { Table } from './tables/table';
+import { TableHeader } from './tables/table-header';
+import { TableCell } from './tables/table-cell';
+import { TableRow } from './tables/table-row';
+
+import { attendees } from './data/attendees';
 
 export function AttendeeList() {
+
   return (
     <div className='flex flex-col gap-4'>
       <div className="flex gap-3 items-center">
@@ -15,77 +24,71 @@ export function AttendeeList() {
         </div>
       </div>
 
-      <div className='border border-white/10 rounded-lg'>
-        <table className='w-full'>
-          <thead>
-            <tr className='border-b border-white/10'>
-              <th
-                style={{ width: 48 }}
-                className='py-3 px-4 text-sm font-semibold text-left'>
-                <input className='bg-black/20 rounded border border-white/18 text-orange-400 outline-none focus:ring-0' type="checkbox" />
-              </th>
-              <th className='py-3 px-4 text-sm font-semibold text-left'>Código</th>
-              <th className='py-3 px-4 text-sm font-semibold text-left'>Participante</th>
-              <th className='py-3 px-4 text-sm font-semibold text-left'>Data de inscrição</th>
-              <th className='py-3 px-4 text-sm font-semibold text-left'>Data do check-in</th>
-              <th style={{ width: 64 }} className='py-3 px-4 text-sm font-semibold text-left'></th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              Array.from({ length: 5 }).map(() => (
-                <tr className='border-b border-white/10 hover:bg-white/5'>
-                  <td className='py-3 px-4 text-sm text-zinc-300'>
-                    <input className='bg-black/20 rounded border border-white/18 text-orange-400 outline-none focus:ring-0' type="checkbox" />
-                  </td>
-                  <td className='py-3 px-4 text-sm text-zinc-300'>12345</td>
-                  <td className='py-3 px-4 text-sm text-zinc-300'>
-                    <div className='flex flex-col gap-1'>
-                      <span className='font-semibold text-zinc-50'>Alisson Souza</span>
-                      <span>binho_alisson@hotmail.com</span>
-                    </div>
-                  </td>
-                  <td className='py-3 px-4 text-sm text-zinc-300'>7 dias atrás</td>
-                  <td className='py-3 px-4 text-sm text-zinc-300'>3 dias atrás</td>
-                  <td className='py-3 px-4 text-sm text-zinc-300'>
-                    <IconButton transparent={true}>
-                      <MoreHorizontal size={16} />
-                    </IconButton>
-                  </td>
-                </tr>
-              ))
-            }
-          </tbody>
-          <tfoot>
-            <tr>
-              <td className='py-3 px-4 text-sm text-zinc-300' colSpan={3}>Mostrando 1 de 200 itens</td>
-              <td
-                className='py-3 px-4 text-sm text-zinc-300 text-right'
-                colSpan={3}
-              >
-                <div className='inline-flex gap-8 items-center'>
-                  <span>Página 1 de 10</span>
-
-                  <div className='flex gap-1.5'>
-                    <IconButton>
-                      <ChevronsLeft size={16} />
-                    </IconButton>
-                    <IconButton>
-                      <ChevronLeft size={16} />
-                    </IconButton>
-                    <IconButton>
-                      <ChevronRight size={16} />
-                    </IconButton>
-                    <IconButton>
-                      <ChevronsRight size={16} />
-                    </IconButton>
+      <Table>
+        <thead>
+          <tr className='border-b border-white/10'>
+            <TableHeader style={{ width: 48 }}>
+              <input className='bg-black/20 rounded border border-white/18 text-orange-400 outline-none focus:ring-0' type="checkbox" />
+            </TableHeader>
+            <TableHeader>Código</TableHeader>
+            <TableHeader>Participante</TableHeader>
+            <TableHeader>Data de inscrição</TableHeader>
+            <TableHeader>Data do check-in</TableHeader>
+            <TableHeader style={{ width: 64 }}></TableHeader>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            attendees.map((attendees) => (
+              <TableRow key={attendees.id}>
+                <TableCell>
+                  <input className='bg-black/20 rounded border border-white/18 text-orange-400 outline-none focus:ring-0' type="checkbox" />
+                </TableCell>
+                <TableCell>{attendees.id}</TableCell>
+                <TableCell>
+                  <div className='flex flex-col gap-1'>
+                    <span className='font-semibold text-zinc-50'>{attendees.name}</span>
+                    <span>{attendees.email}</span>
                   </div>
+                </TableCell>
+                <TableCell>{formatRelative(attendees.createdAt, new Date(), { locale: ptBR, })}</TableCell>
+                <TableCell>{formatRelative(attendees.checkedInAt, new Date(), { locale: ptBR, })}</TableCell>
+                <TableCell>
+                  <IconButton transparent={true}>
+                    <MoreHorizontal size={16} />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))
+          }
+        </tbody>
+        <tfoot>
+          <tr>
+            <TableCell colSpan={3}>Mostrando 1 de 200 itens</TableCell>
+            <TableCell className='text-right' colSpan={3}
+            >
+              <div className='inline-flex gap-8 items-center'>
+                <span>Página 1 de 10</span>
+
+                <div className='flex gap-1.5'>
+                  <IconButton>
+                    <ChevronsLeft size={16} />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronLeft size={16} />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronRight size={16} />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronsRight size={16} />
+                  </IconButton>
                 </div>
-              </td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
+              </div>
+            </TableCell>
+          </tr>
+        </tfoot>
+      </Table>
     </div>
   )
 }
